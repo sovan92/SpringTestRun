75dfa1d8292993e7fab871b72dd197dab9d73e88
Sovan Rath <sovanrath@Sovans-MacBook-Pro.local>
2024-03-09 22:20:55 -0800

    updated the references for the 1st commit

---
+++ src/main/java/com/example/springtestrun/SpringTestRunApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtestrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestRunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springtestrun/SpringTestRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtestrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestRunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
